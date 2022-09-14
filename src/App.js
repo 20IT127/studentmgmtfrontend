c1c8280a54f3facec1e43fe97ea69a5e8c11249d
20IT127-HUTISH-SEJU <111727424+20IT127@users.noreply.github.com>
2022-09-14 13:46:36 +0530

    Added Delete.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,13 @@ import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
-import Footer from './components/Footer';
-import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-
   return (
     <div className="App">
-      <Router>
       <NavigationBar/>
-      <Container>
-        <Routes>
-      <Route path="student" element={<Student/>}/>
-      <Route path="listStudents" element={<StudentList/>}/>
-      </Routes>
-      </Container>
-      <Footer/>
-      </Router>
+      <Student/>
+      <StudentList/>
     </div>
   );
 }
