e8a9752ab48d1af71da0fba754c6047ac0b38c7f
20IT127-HUTISH-SEJU <111727424+20IT127@users.noreply.github.com>
2022-09-14 13:52:57 +0530

    Added Students Listing.

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
+import { Card, Container, Table } from "react-bootstrap";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-import {Link} from 'react-router-dom'
 
-export default function StudentList(props) {
+export default function StudentList() {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
@@ -18,17 +15,6 @@ export default function StudentList(props) {
       .then((response) => setStudents(response.data))
       .catch((error) => alert(error));
   };
-
-  let deleteStudent = (studentId) => {
-    axios.delete("http://localhost:8080/student/"+studentId)
-    .then(response=> {
-      if (response.data !== null){
-        props.showAlert("success", "Record deleted successfully")
-        setStudents(students.filter(student=>student.id!==studentId));
-      }
-    })
-  }
-
   return (
     <div className="my-3">
       <Container>
@@ -42,7 +28,6 @@ export default function StudentList(props) {
                 <th>Student Id</th>
                 <th>Student Name</th>
                 <th>Student Address</th>
-                <th>Edit/Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -52,17 +37,10 @@ export default function StudentList(props) {
                 </tr>
               ) : (
                 students.map((student)=>
-                <tr key={student.id}>
+                <tr>
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
-                  <td>
-                    <ButtonGroup>
-                      <Link to={"/student/"+student.id}><Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button></Link>{ ' '}
-                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
-                      {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
-                    </ButtonGroup>
-                  </td>
                 </tr>
                 )
               )}
